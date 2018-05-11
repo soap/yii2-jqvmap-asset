6f026191df9a72d8691cb9bceda8501406c99ca4
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2018-05-11 11:17:34 +0700

    Add world map

--- src/assets/jQvmapWorld.php
+++ src/assets/jQvmapWorld.php
@@ -3,26 +3,22 @@
  * Created by PhpStorm.
  * User: soap
  * Date: 11/05/2018
- * Time: 10:18
+ * Time: 11:13
  */
 
 namespace soap\jqvmap\assets;
 
 use yii\web\AssetBundle;
 
-class jQvmapAsset extends AssetBundle
+class jQvmapWorldAsset extends AssetBundle
 {
     public $sourcePath = '@bower/jqvmap/dist';
 
     public $js = [
-        'jquery.vmap.js'
-    ];
-
-    public $css = [
-        'jqvmap.css'
+        'maps/jquery.vmap.world.js'
     ];
 
     public $depends = [
-        'yii\web\JqueryAsset',
+        'soap\jqvmap\assets\jQvmapAsset'
     ];
 }
